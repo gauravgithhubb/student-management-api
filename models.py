329c171af51a3de79ba76a941aead0d8f8485c74
gauravgithhubb <gauraj555@gmail.com>
2026-06-28 12:16:32 +0530

    Added user model and schemas

--- models.py
+++ models.py
@@ -9,3 +9,11 @@ class Student(Base):
     branch = Column(String, nullable=False)
     year = Column(Integer, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
--- schemas.py
+++ schemas.py
@@ -14,3 +14,16 @@ class StudentResponse(StudentBase):
 
     class Config:
         from_attributes = True
+
+class UserCreate(BaseModel):
+    name: str = Field(..., min_length=3, max_length=50)
+    email: EmailStr
+    password: str = Field(..., min_length=6)
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
+    class Config:
+        from_attributes = True
