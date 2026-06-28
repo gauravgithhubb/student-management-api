cf0990864e609bf5622c8696f1c6100a2587a532
gauravgithhubb <gauraj555@gmail.com>
2026-06-28 11:59:02 +0530

    Added email validation

--- schemas.py
+++ schemas.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 class StudentBase(BaseModel):
-    name: str
-    branch: str
-    year: int
-    email: str
+    name: str = Field(..., min_length=3, max_length=50)
+    branch: str = Field(..., min_length=2, max_length=50)
+    year: int = Field(..., ge=1, le=4)
+    email: EmailStr
 
 class StudentCreate(StudentBase):
     pass
