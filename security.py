958a67fc0a4f4c0d33c7710cdc20078cfc692b00
gauravgithhubb <gauraj555@gmail.com>
2026-06-28 18:49:43 +0530

    Added environment variable configuration

--- security.py
+++ security.py
@@ -1,9 +1,18 @@
-from jose import jwt
+import os
+from dotenv import load_dotenv
+from jose import jwt, JWTError
 from datetime import datetime, timedelta
+from fastapi import HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
-SECRET_KEY = "mysecretkey123"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+security = HTTPBearer()
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -14,11 +23,6 @@ def create_access_token(data: dict):
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-from fastapi import HTTPException, Depends
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from jose import JWTError
-
-security = HTTPBearer()
 
 def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
